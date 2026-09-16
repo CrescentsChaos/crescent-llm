a07f95d929180d0637df9e11e482d70a10f96dee
Crescent <trtonoy2@gmail.com>
2026-09-16 15:46:28 +0600

    initial update

--- model/transformer.py
+++ model/transformer.py
@@ -62,3 +62,59 @@ class InputEmbedding(nn.Module):
         position_vectors = self.position_embedding(tokens)
 
         return token_vectors + position_vectors
+class SelfAttention(nn.Module):
+
+    def __init__(self, embedding_dim, context_length):
+        super().__init__()
+
+        self.query = nn.Linear(
+            embedding_dim,
+            embedding_dim
+        )
+
+        self.key = nn.Linear(
+            embedding_dim,
+            embedding_dim
+        )
+
+        self.value = nn.Linear(
+            embedding_dim,
+            embedding_dim
+        )
+
+        # Create a lower-triangular mask
+        self.register_buffer(
+            "mask",
+            torch.tril(
+                torch.ones(
+                    context_length,
+                    context_length
+                )
+            )
+        )
+
+    def forward(self, x):
+
+        Q = self.query(x)
+        K = self.key(x)
+        V = self.value(x)
+
+        # Calculate attention scores
+        scores = Q @ K.transpose(-2, -1)
+
+        # Get current sequence length
+        sequence_length = x.size(1)
+
+        # Hide future tokens
+        scores = scores.masked_fill(
+            self.mask[
+                :sequence_length,
+                :sequence_length
+            ] == 0,
+            float("-inf")
+        )
+        attention_weights = torch.softmax(
+    scores,dim=-1
+)
+
+        return Q, K, V, scores, attention_weights
--- test_model.py
+++ test_model.py
@@ -1,13 +1,21 @@
 import torch
 
 from config import ModelConfig
-from model.transformer import InputEmbedding
+from model.transformer import (
+    InputEmbedding,
+    SelfAttention
+)
 
 
 embedding = InputEmbedding(
     vocab_size=ModelConfig.vocab_size,
     context_length=ModelConfig.context_length,
     embedding_dim=ModelConfig.embedding_dim
+)
+
+attention = SelfAttention(
+    embedding_dim=ModelConfig.embedding_dim,
+    context_length=ModelConfig.context_length
 )
 
 
@@ -17,20 +25,25 @@ tokens = torch.tensor([
 ])
 
 
-output = embedding(tokens)
+x = embedding(tokens)
+
+Q, K, V, scores = attention(x)
 
 
-print("Input shape:")
-print(tokens.shape)
+print("Input:")
+print(x.shape)
 
-print("\nOutput shape:")
-print(output.shape)
+print("\nQuery:")
+print(Q.shape)
 
-print("\nNumber of parameters:")
+print("\nKey:")
+print(K.shape)
 
-print(
-    sum(
-        parameter.numel()
-        for parameter in embedding.parameters()
-    )
-)
+print("\nValue:")
+print(V.shape)
+
+print("\nAttention scores:")
+print(scores.shape)
+
+print("\nFirst example's scores:")
+print(scores[0])
